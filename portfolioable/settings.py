ed1ed3885814c5b25f73d492e90da6e804b281b5
Ashley Benson <bensona2@spu.edu>
2022-08-11 14:52:34 -0700

    update BE for monthly paymen_2t

--- portfolioable/settings.py
+++ portfolioable/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=f4zmg_#+ajodotw9_6z^rzs_(oo8tkqllqcrcb9%g_n**#_de'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
